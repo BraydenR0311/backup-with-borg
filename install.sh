3bf4126b9bafddc9afbd32f923a07a120e3700e6
brayden <brayden.riesberg@aol.com>
2025-05-24 21:51:54 -0500

    ensure backupdir logic

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-mkdir $HOME/.backups
+BACKUP_DIR=$HOME/.backups/
+
+if [[ ! -d $BACKUP_DIR ]]; then
+	mkdir $HOME/.backups
+fi
 
 sudo cp -v ./backup.sh $HOME/.local/bin/
 sudo chmod u+x $HOME/.local/bin/backup.sh
@@ -13,3 +17,5 @@ sudo cp -v ./backup.timer $HOME/.config/systemd/user/
 sudo systemctl daemon-reload
 systemctl --user enable backup.timer
 systemctl --user start backup.timer
+
+borg init -e repokey $BACKUP_DIR
